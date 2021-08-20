e76ecd3be0ab1e57323134fc509cc87d20928036
Tatay2002 <88531466+Tatay2002@users.noreply.github.com>
2021-08-20 19:47:12 +0700

    EX2

--- Ex2/2008_7.py
+++ Ex2/2008_7.py
@@ -2,12 +2,13 @@ First_number = float(input("Enter the first number : "))
 MathematicsSign = input("Enter the mathematics sign : ")
 Second_number = float(input("Enter the second number : "))
 if(MathematicsSign == '+'):
-    print("The answer is ", First_number + Second_number)
+    print("The answer is ", round((First_number + Second_number), 2))
 elif(MathematicsSign == '-'):
-    print("The answer is ", First_number - Second_number)
+    print("The answer is ", round((First_number - Second_number),2))
 elif (MathematicsSign == '*'):
-    print("The answer is ", First_number * Second_number)
+    print("The answer is ", round((First_number * Second_number),2))
 elif (MathematicsSign == '/'):
-    print("The answer is ", First_number / Second_number)
+    print("The answer is ", round((First_number / Second_number),2))
 elif(MathematicsSign == '%'):
-    print("The answer is ", First_number % Second_number)
+    print("The answer is ", round((First_number % Second_number),2))
+
